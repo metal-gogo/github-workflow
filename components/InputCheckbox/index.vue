45f2dea4ec8c8b7817836cb40dcf6aa19691e5f6
metal-gogo <d.g.g69@me.com>
2019-10-28 01:05:12 -0600

    ✨ Created InputCheckbox component

---
+++ components/InputCheckbox/index.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="input-checkbox__container">
+    <input
+      :id="id"
+      :name="name"
+      type="checkbox"
+      class="input-checkbox"
+      @input="handleInput"
+    />
+    <label :for="id" class="input-checkbox__label">
+      {{ label }}
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+    label: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    value: {
+      type: String,
+      required: false,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      inputValue: this.value
+    }
+  },
+  methods: {
+    handleInput(e) {
+      this.inputValue = e.target.value
+      this.$emit('input', this.inputValue)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.input-checkbox__container {
+  display: flex;
+  align-items: center;
+  margin-bottom: 1.25rem;
+}
+
+.input-checkbox {
+  width: 1.25rem;
+  height: 1.25rem;
+  margin-right: 0.5rem;
+  opacity: 0;
+
+  &:focus {
+    + .input-checkbox__label {
+      &::before {
+        box-shadow: 0 0 0 3px $sunflower;
+      }
+    }
+  }
+
+  &:checked {
+    + .input-checkbox__label {
+      &::after {
+        content: '';
+      }
+    }
+  }
+}
+
+.input-checkbox__label {
+  position: relative;
+  display: flex;
+  align-items: center;
+
+  &::before {
+    position: absolute;
+    left: -1.75rem;
+    height: 1.25rem;
+    width: 1.25rem;
+    content: '';
+    border: 2px solid $violet-heart;
+    border-radius: 3px;
+  }
+
+  &::after {
+    position: absolute;
+    left: calc(-1.5rem + 1px);
+    height: 0.5rem;
+    width: 0.75rem;
+    content: none;
+    border-left: 2px solid;
+    border-bottom: 2px solid;
+    border-color: $violet-heart;
+    transform: rotate(-45deg) translateY(-2px);
+  }
+}
+</style>
